6d68f36716da19daec75bc0eeaebd68d964d8595
hard1x <112309685+hard1x0-0@users.noreply.github.com>
2025-10-12 12:30:45 +0530

    nothing new to commit, working tree clean

---
+++ hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main () {
+    // cout << "hello world" << endl;
+    int fav_num;
+    // cout << "print random number" << endl;
+    cin >> fav_num; 
+    cout << "bkl"<<endl;
+    return 0;
+}
